903723399a59ce755322e2e208975d1b34c733e7
as7118618-web <as7118618@gmail.com>
2026-09-23 22:09:02 +0530

    Add Kivy app for attendance functionality

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+from kivy.app import App
+from kivy.uix.label import Label
+
+class AttendanceApp(App):
+    def build(self):
+        return Label(text='Attendance App is Running!')
+
+if __name__ == '__main__':
+    AttendanceApp().run()
